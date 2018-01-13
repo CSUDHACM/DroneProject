7705c7d26fbfb605dc50e7fae9a3b8d8c595f6e8
Alvaro Tzul <tzulalvaro@gmail.com>
2018-01-13 07:22:42 -0800

    Update Test.ino

--- AutonomousDrone/Test.ino
+++ AutonomousDrone/Test.ino
@@ -1,19 +1,16 @@
 #include <CoDrone.h>
 
-
-
 void setup(){
   
   CoDrone.begin(115200);
   CoDrone.AutoConnect(NearbyDrone);
-  
   
   CoDrone.FlightEvent(TakeOff);
   delay(9000);
   CoDrone.FlightEvent(Landing);
   
 }
-6
+
 void loop(){
   //CoDrone.FlightEvent(Landing);
 }
