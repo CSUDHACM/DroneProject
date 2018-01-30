f5c051c075492b27d616b8d23d4eae4b4785e115
msanchezperez1 <35042556+msanchezperez1@users.noreply.github.com>
2018-01-30 15:37:46 -0800

    Add files via upload

---
+++ DroneFeatures/Juan_Codrone.ino
@@ -0,0 +1,22 @@
+#include <CoDrone.h>
+
+void setup() {
+ CoDrone.begin(115200);
+ CoDrone.AutoConnect(NearbyDrone);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    if(digitalRead(18)){
+      CoDrone.FlightEvent(TakeOff);
+      delay(250);
+      CoDrone.Control();
+    }
+    if(digitalRead(11)&&digitalRead(18)){
+      CoDrone.FlightEvent(Landing);
+    }
+    if(digitalRead(11)&&digitalRead(14)&&digitalRead(18)){
+      CoDrone.FlightEvent(Stop);
+    }
+}
