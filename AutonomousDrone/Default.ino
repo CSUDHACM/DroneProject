794d65051e3b3859978b40eab4792742bcae8872
darkhall9 <kevinramirez266@yahoo.com>
2018-04-06 16:09:28 -0700

    Update Default.ino

--- AutonomousDrone/Default.ino
+++ AutonomousDrone/Default.ino
@@ -4,43 +4,33 @@ void setup() {
   CoDrone.begin(115200);
   CoDrone.AutoConnect(NearbyDrone); //Creates connection to Nearby Drone
   CoDrone.DroneModeChange(Flight);  //Sets control to Drone mode
-  CoDrone.FlightEvent(TakeOff);     //Drone takes off for 2 seconds
-  delay(2000);
-  CoDrone.FlightEvent(Landing);     //Drone lands
-  //delay(500);
-  //CoDrone.LedColor(EyeHold, Gold); //Changes color of the eye to Gold
+//  CoDrone.FlightEvent(TakeOff);     //Drone takes off for 2 seconds
+//  CoDrone.FlightEvent(Landing);     //Drone lands
 }
 
 void loop() {
-  byte Sen11 = digitalRead(11);   //Checks the sensors for input (Covering said sensors gives input)
-  byte Sen14 = digitalRead(14);
-  byte Sen18 = digitalRead(18);
+//  byte Sen11 = digitalRead(11);   //Checks the sensors for input (Covering said sensors gives input)
+//  byte Sen14 = digitalRead(14);
+//  byte Sen18 = digitalRead(18);
 
-  if(Sen11 && Sen18) { //Emergency stop 
+  if(digitalRead(11)) { //Emergency stop 
     CoDrone.FlightEvent(Stop);
-    CoDrone.Buzz(8000,2);
+    CoDrone.Buzz(8000,1);
   }
-  else if(Sen11) {
-    CoDrone.FlightEvent(Landing); //Lands Drone if Sensor 11 has an input.
-  }
-  else if(Sen18) { 
-    rotate();
-  }    
+//  else if(digitalRead(18)) {  //Maria Perez Segment of code.
+//    for(int i = 0; i < 4; i++) {  
+//      YAW = 90;
+//      delay(250);
+//    }
+//    CoDrone.Control();
+//  }                 //End of Maria Perez Segment.
 
-  if (PAIRING == true) //Takes input from left and right analog sticks for flight control.
+  else if (PAIRING == true) //Takes input from left and right analog sticks for flight control.
   {
     YAW = -1 * CoDrone.AnalogScaleChange(analogRead(A3)); 
     THROTTLE  = CoDrone.AnalogScaleChange(analogRead(A4)); 
     ROLL = -1 * CoDrone.AnalogScaleChange(analogRead(A5)); 
     PITCH = CoDrone.AnalogScaleChange(analogRead(A6)); 
-    CoDrone.Control(SEND_INTERVAL); //Sends analog input to the drone for execution.
+    CoDrone.Control(); //Sends analog input to the drone for execution.
   }
-}
-
-void rotate() { //Maria Perez Method
-  for(int i = 0; i < 4; i++) {
-    YAW = 90;
-    delay(250);
-  }
-  CoDrone.Control();
 }
