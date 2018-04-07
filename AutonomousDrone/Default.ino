6d1134017ca7705e1c2b60ff78e8ae6f5f7d1e53
Alvaro Tzul <tzulalvaro@gmail.com>
2018-04-06 17:03:47 -0700

    Update Default.ino

--- AutonomousDrone/Default.ino
+++ AutonomousDrone/Default.ino
@@ -3,6 +3,7 @@
 void setup() {
   CoDrone.begin(115200);
   CoDrone.AutoConnect(NearbyDrone); //Creates connection to Nearby Drone
+  CoDrone.Buzz(4000,1);
   CoDrone.DroneModeChange(Flight);  //Sets control to Drone mode
 //  CoDrone.FlightEvent(TakeOff);     //Drone takes off for 2 seconds
 //  CoDrone.FlightEvent(Landing);     //Drone lands
@@ -13,9 +14,12 @@ void loop() {
 //  byte Sen14 = digitalRead(14);
 //  byte Sen18 = digitalRead(18);
 
+  irSensors();
+
   if(digitalRead(11)) { //Emergency stop 
     CoDrone.FlightEvent(Stop);
     CoDrone.Buzz(8000,1);
+    CoDrone.Control(SEND_INTERVAL); //Sends analog input to the drone for execution.
   }
 //  else if(digitalRead(18)) {  //Maria Perez Segment of code.
 //    for(int i = 0; i < 4; i++) {  
@@ -32,5 +36,20 @@ void loop() {
     ROLL = -1 * CoDrone.AnalogScaleChange(analogRead(A5)); 
     PITCH = CoDrone.AnalogScaleChange(analogRead(A6)); 
     CoDrone.Control(SEND_INTERVAL); //Sends analog input to the drone for execution.
+    
+    
   }
+}
+void irSensors(){
+  ///////////////IR SENSORS ON BACK/////////////// 
+
+  if(digitalRead(12)){//spins if you hold the 12 sensor
+   for(int i = 0; i < 4; i++){
+      YAW = 90;//with the speed of 90
+      delay(250);// 1/4       OF A SECOND
+    }
+    
+  }
+CoDrone.Control(SEND_INTERVAL); //Sends analog input to the drone for execution.
+  
 }
